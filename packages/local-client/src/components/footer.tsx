7241e3827b9191d65d47132e09e7112ba8179e4c
Raghav Kattel <arg.ahv7@gmail.com>
2022-03-18 16:35:39 +0545

    inital readme added

---
+++ packages/local-client/src/components/footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer:React.FC = () => {
+    return <div>
+        <a href="https://raghavkattel.com.np/projects">
+            View More Projects
+        </a>
+    </div>
+}
+
+export default Footer
--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -4,12 +4,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./state";
 import CellList from "./components/cell-list";
+import Footer from "./components/footer";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
         <CellList />
+      </div>
+      <div>
+        <Footer />
       </div>
     </Provider>
   );
